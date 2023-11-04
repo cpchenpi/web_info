b2873fd66310f1148fb792702668d1fe5b371b0c
cpchenpi <cmtclx@163.com>
2023-11-04 10:54:27 +0800

    update split using synonym

--- lab1/split_words.py
+++ lab1/split_words.py
@@ -8,6 +8,15 @@ with open("lab1/cn_stopwords.txt", "r", encoding="utf-8") as stopwords_file:
 
 thulac = thulac.thulac(seg_only=True, filt=True)
 
+synonym_dict = dict()
+
+with open("lab1/dict_synonym.txt", "r", encoding="utf-8") as synonym_file:
+    for line in synonym_file.readlines():
+        ls = line.split()[1:]
+        if len(ls) >= 2:
+            for i in range(1, len(ls)):
+                synonym_dict[ls[i]] = ls[0]
+
 
 def split_words(id: int, method="jieba"):
     data = json.load(open("lab1/book_data/" + str(id) + ".json", "r", encoding="utf-8"))
@@ -24,8 +33,15 @@ def split_words(id: int, method="jieba"):
     for key in ["translator", "author"]:
         for name in data[key]:
             ret.add(name)
+    ret = set(
+        map(
+            lambda word: synonym_dict[word] if word in synonym_dict else word,
+            ret,
+        )
+    )
     return ret - stopwords_set
 
 
-print(split_words(1000280, method="jieba"))
-print(split_words(1000280, method="thulac"))
+if __name__ == "__main__":
+    print(split_words(1000280, method="jieba"))
+    print(split_words(1000280, method="thulac"))
